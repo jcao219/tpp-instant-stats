08a4118b0835b5172c27d1cb33f26885b7f12065
Jimmy Cao <jcao219@gmail.com>
2014-02-23 06:09:01 -0600

    Make it so that tampermonkey is supported, maybe we can package this as a chrome extension too

--- chat_stats.user.js
+++ chat_stats.user.js
@@ -54,54 +54,65 @@ $("<span style='font-weight:bold;'>Past "+MAX_INPUT_HISTORY+" Commands</span>"+
 "<span id='custom-stats'/>").insertAfter(top_thing);
 
 // --- Main ---
+var initialize_stats = function() {
+    var inputCounts = {
+        up: 0,
+        down: 0,
+        left: 0,
+        right: 0,
+        a: 0,
+        b: 0,
+        start: 0,
+        democracy: 0,
+        anarchy: 0
+    };
 
-var inputCounts = {
-    up: 0,
-    down: 0,
-    left: 0,
-    right: 0,
-    a: 0,
-    b: 0,
-    start: 0,
-    democracy: 0,
-    anarchy: 0
-};
+    // keep track of past inputs so that we can erase the oldest inputs
+    var pastInputs = [];
 
-// keep track of past inputs so that we can erase the oldest inputs
-var pastInputs = [];
+    // keep a copy of the old insert_chat_line method so we can call it
+    var __icl = myWindow.CurrentChat.insert_chat_line;
 
-// keep a copy of the old insert_chat_line method so we can call it
-var __icl = myWindow.CurrentChat.insert_chat_line;
-
-// replace the insert_chat_line method
-myWindow.CurrentChat.insert_chat_line = function(e) { 
-    for(var button in inputCounts) {
-        if(myWindow.CurrentChat.format_message(e).trim().match(new RegExp("^" + button + "$", "i"))) {
-            inputCounts[button]++;
-            // erase everything older than MAX_INPUT_HISTORY
-            pastInputs.push(button);
-            while(pastInputs.length > MAX_INPUT_HISTORY)
-                inputCounts[pastInputs.shift()]--;
-            break;
+    // replace the insert_chat_line method
+    myWindow.CurrentChat.insert_chat_line = function(e) { 
+        for(var button in inputCounts) {
+            if(e.message.trim().match(new RegExp("^" + button + "$", "i"))) {
+                inputCounts[button]++;
+                // erase everything older than MAX_INPUT_HISTORY
+                pastInputs.push(button);
+                while(pastInputs.length > MAX_INPUT_HISTORY)
+                    inputCounts[pastInputs.shift()]--;
+                break;
+            }
         }
-    }
-    // call the original insert_chat_line method
-    __icl.apply(CurrentChat, [e]);
-};
+        // call the original insert_chat_line method
+        __icl.apply(CurrentChat, [e]);
+    };
+    
+    // update display every 1000ms
+    myWindow.setInterval(function() {
+        var stats_str = []; 
+        $.each(inputCounts, function(button, count) { 
+            stats_str.push(button + ": " + count);
+        });
+        $("#input-stats").html(stats_str.join(", "));
 
-// update display every 1000ms
-myWindow.setInterval(function() {
-    var stats_str = []; 
-    $.each(inputCounts, function(button, count) { 
-        stats_str.push(button + ": " + count);
-    });
-    $("#input-stats").html(stats_str.join(", "));
+        // try to evaluate the custom code, with the inputCounts as the context
+        var custom_code = $("#custom-stats-code").val();
+        try {$("#custom-stats").html("&nbsp" + ((
+            new Function("with(this) { return " + custom_code + "; }")).call(inputCounts)).toString());
+        }
+        catch(e) {$("#custom-stats").html("&nbsp;invalid expression");}
+    }, 1000);
+}
 
-    // try to evaluate the custom code, with the inputCounts as the context
-    var custom_code = $("#custom-stats-code").val();
-    try {$("#custom-stats").html("&nbsp" + ((
-        new Function("with(this) { return " + custom_code + "; }")).call(inputCounts)).toString());
-    }
-    catch(e) {$("#custom-stats").html("&nbsp;invalid expression");}
-}, 1000);
+$(function(){
+	//Instead of testing for the existence of CurrentChat, check if the spinner is gone.
+	var chatLoadedCheck = myWindow.setInterval(function () {
+		if($("#chat_loading_spinner").css('display') == 'none'){
+			myWindow.clearInterval(chatLoadedCheck);
+			initialize_stats();
+		}
+	}, 100);
+});
 })();
