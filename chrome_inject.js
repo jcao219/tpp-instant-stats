e3e8b58ccd1f83e78db324a2e6393fa1ebbea127
Jimmy Cao <jcao219@gmail.com>
2014-02-23 18:08:41 -0600

    Removing the chrome extension files and moving them to a different repo.

--- chrome_inject.js
+++
@@ -1,6 +0,0 @@
-var s = document.createElement('script');
-s.src = chrome.extension.getURL("chat_stats.user.js");
-s.onload = function() {
-    this.parentNode.removeChild(this);
-};
-document.head.appendChild(s);
